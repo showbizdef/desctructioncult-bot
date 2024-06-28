0ac607a0a409f95a5bfebe88ee2f0eb45c7fa29d
Maxim Sitowskyi <sitowskyimaxim@gmail.com>
2024-06-28 23:38:38 +0300

    first version

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv/config')
+
+const {BOT_TOKEN} = process.env
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const token = '7372657704:AAH-pdMHX2Tu5Fk3EBfZ5tKnKNNSPnIU-3w'
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(BOT_TOKEN, {polling: true});
 
 const firstNames = ["Amaterasu", "Sora", "Emperor", "Hiroshi", "Amethyst", "Playada", "Adaptive", "Rebellious", "Yamato", "Dagon", "Katsu", "Saint", "Chrome", "Manera", "Arata", "Phantom", "Mamora", "Shirou", "Infused", "Jeffrey", "Shadow", "Neri"];
 const lastNames = ["Edge", "Tatsuki", "Cartier", "Explorer", "Blacksimens", "Cult", "Castle", "Bennett", "Cho", "Northside", "Eternal", "Devilside", "Destruction", "Murasaki", "Violence", "Recovery", "Armano", "Takeda", "Soyama", "Hellwalker"];
@@ -14,7 +16,7 @@ function generateNickname() {
 }
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome! Send /generate to get a nickname.");
+    bot.sendMessage(msg.chat.id, "Привет!👋\nДля того, чтобы сгенерировать свой никнейм, введите команду /generate");
 });
 
 bot.onText(/\/generate/, (msg) => {
@@ -24,7 +26,7 @@ bot.onText(/\/generate/, (msg) => {
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    if (msg.text !== '/start' && msg.text !== '/generate') {
+    if (msg.text !== '/start' && msg.text !== '/generate    ') {
         bot.sendMessage(chatId, 'Пропишите команду /generate для получения никнейма.')
     }
 });
