51b47336b3cf6a55eeaece48af070c6e838fc524
Maxim Sitowskyi <sitowskyimaxim@gmail.com>
2024-07-05 23:04:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,59 @@ function generateNickname() {
     return `${randomFirstName}_${randomLastName}`;
 }
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Привет!👋\nДля того, чтобы сгенерировать свой никнейм, введите команду /generate");
+async function getOnline(serverId) {
+    try {
+        const response = await axios.get('https://backend.arizona-rp.com/server/get-all', {
+            headers: {
+                'Referer': 'https://arizona-rp.com/'
+            }
+        });
+
+
+        const data = responce.data;
+        return data[serverId].players
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+bot.onText(/\start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Welcome! Use the buttons below to interact with the bot.", {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Generate Nickname', callback_data: 'generate'}],
+                [{text: 'Get Online Info', callback_data: 'online_0'}]
+            ]
+        }
+    });
 });
 
-bot.onText(/\/generate/, (msg) => {
-    const nickname = generateNickname();
-    bot.sendMessage(msg.chat.id, `Ваш никнейм для создания никнейма в игре: ${nickname}`)
+bot.on('callback_query', async (callbackQuery) => {
+    const msg = callbackQuery.message;
+    const data = callbackQuery.data;
+
+    if (data === 'generate') {
+        const nickname = generateNickname();
+        bot.sendMessage(msg.chat.id, `Your GTA SAMP nickname is: ${nickname}`);
+    } else if (data.startWith('online_')) {
+        const serverId = parseInt(data.split('_')[1], 10);
+        const players = await getOnline(serverId);
+
+        if (players !== null) {
+            bot.sendMessage(msg.chat.id, `Server ${serverId} has ${players} players online.`);   
+        } else {
+            bot.sendMessage(msg.chat.id, `Failed to fetch online data for server ${serverId}.`);
+        }
+    }
+
+    bot.answerCallbackQuery(callbackQuery.id);
 });
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    if (msg.text !== '/start' && msg.text !== '/generate') {
-        bot.sendMessage(chatId, 'Пропишите команду /generate для получения никнейма.')
+    if (!msg.text.startsWith('/start') && !msg.text.startsWith('/generate') && !msg.text.startsWith('/online')) {
+       bot.sendMessage(chatId, 'Use the buttons to interact with the bot or send /start to interact with the bot or send /start to see options.'); 
     }
 });
